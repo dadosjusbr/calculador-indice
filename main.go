414ee8666d49675060c089a5851347cb0df58016
Daniel Fireman <danielfireman@gmail.com>
2022-03-08 21:35:34 -0300

    corringindo saída do estágio (resultado de coleta)

--- main.go
+++ main.go
@@ -8,31 +8,28 @@ import (
 	"github.com/dadosjusbr/coletores/status"
 	"github.com/dadosjusbr/indice"
 	"github.com/dadosjusbr/proto/coleta"
-	"github.com/dadosjusbr/proto/pipeline"
 	"google.golang.org/protobuf/encoding/prototext"
 )
 
 func main() {
-	// Processa entrada.
-	var er pipeline.ResultadoExecucao
-	er.Rc = new(coleta.ResultadoColeta)
-
-	erIN, err := ioutil.ReadAll(os.Stdin)
+	// Processa entrada que vem do coletor, que é um resultado de coleta.
+	rcIn, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
 		status.ExitFromError(status.NewError(4, fmt.Errorf("error reading crawling result: %q", err)))
 	}
-	if err = prototext.Unmarshal(erIN, er.Rc); err != nil {
-		status.ExitFromError(status.NewError(5, fmt.Errorf("error unmarshaling crawling resul from STDIN: %q\n\n %s ", err, string(erIN))))
+	var rc coleta.ResultadoColeta
+	if err = prototext.Unmarshal(rcIn, &rc); err != nil {
+		status.ExitFromError(status.NewError(5, fmt.Errorf("error unmarshaling crawling resul from STDIN: %q\n\n %s ", err, string(rcIn))))
 	}
 
 	// Calcula índice e atualiza proto.
-	score := indice.CalcScore(*er.Rc.Metadados)
-	er.Rc.Metadados.IndiceCompletude = float32(score.CompletenessScore)
-	er.Rc.Metadados.IndiceFacilidade = float32(score.EasinessScore)
-	er.Rc.Metadados.IndiceTransparencia = float32(score.Score)
+	score := indice.CalcScore(*rc.Metadados)
+	rc.Metadados.IndiceCompletude = float32(score.CompletenessScore)
+	rc.Metadados.IndiceFacilidade = float32(score.EasinessScore)
+	rc.Metadados.IndiceTransparencia = float32(score.Score)
 
-	// Imprime proto atualizado.
-	b, err := prototext.Marshal(&er)
+	// Imprime resultado de coleta atualizado.
+	b, err := prototext.Marshal(&rc)
 	if err != nil {
 		err = status.NewError(status.Unknown, fmt.Errorf("error marshalling execution result with score:%w", err))
 		status.ExitFromError(err)
