cbe6e38dfb846c25f09ff4a5fd0852c03eda1911
Joellensilva <joellen.absilva@gmail.com>
2022-10-26 17:18:55 -0300

    alterando

--- main.go
+++ main.go
@@ -21,19 +21,19 @@ func main() {
 	if err = prototext.Unmarshal(rcIn, &rc); err != nil {
 		status.ExitFromError(status.NewError(5, fmt.Errorf("error unmarshaling crawling resul from STDIN: %q\n\n %s ", err, string(rcIn))))
 	}
-
-	// Calcula índice e atualiza proto.
-	score := indice.CalcScore(*rc.Metadados)
-	rc.Metadados.IndiceCompletude = float32(score.CompletenessScore)
-	rc.Metadados.IndiceFacilidade = float32(score.EasinessScore)
-	rc.Metadados.IndiceTransparencia = float32(score.Score)
-
+	// Define se o formato é aberto
 	extensions := []string{"PDF", "ODS", "JSON", "CSV", "HTML", "ODT"}
 	for _, extensao := range extensions {
 		if rc.Metadados.Extensao.String() == extensao {
 			rc.Metadados.FormatoAberto = true
 		}
 	}
+	// Calcula índice e atualiza proto.
+	score := indice.CalcScore(*rc.Metadados)
+	rc.Metadados.IndiceCompletude = float32(score.CompletenessScore)
+	rc.Metadados.IndiceFacilidade = float32(score.EasinessScore)
+	rc.Metadados.IndiceTransparencia = float32(score.Score)
+
 	// Imprime resultado de coleta atualizado.
 	b, err := prototext.Marshal(&rc)
 	if err != nil {
